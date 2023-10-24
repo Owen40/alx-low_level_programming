e7f8e4892031a27ad261e6b605e4ce2aeb67e746
Owen40 <omuthami27@gmail.com>
2023-10-24 11:18:43 -0700

    Variables commit

---
+++ 0x01-variable_if_else_while/1-lats_digit.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ *  * main - Entry point
+ *   *
+ *    * Description: This program generates a random number and prints its last digit
+ *     *              along with a description.
+ *      *
+ *       * Return: Always 0 (Success)
+ *        */
+int main(void)
+{
+	    int n;
+
+	        srand(time(0));
+		    n = rand() - RAND_MAX / 2;
+
+		        printf("Last digit of %d is %d and is ", n, abs(n) % 10);
+
+			    if (abs(n) % 10 > 5)
+				        {
+						        printf("greater than 5\n");
+							    }
+			        else if (abs(n) % 10 == 0)
+					    {
+						            printf("0\n");
+							        }
+				    else
+					        {
+							        printf("less than 6 and not 0\n");
+								    }
+
+				        return (0);
+}
